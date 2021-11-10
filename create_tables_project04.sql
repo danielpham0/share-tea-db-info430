12a2cf5afc72d966976a137502b64650a2235b7a
Jonathan <omgitsjt@uw.edu>
2021-11-10 14:58:51 -0800

    Added Employee_Type, Employee, Gender, Store, Shift_Type, Shift, Shift_Employee tables

--- create_tables_project04.sql
+++ create_tables_project04.sql
@@ -77,3 +77,95 @@ GO
 --    [SizeID] [int] NOT NULL FOREIGN KEY REFERENCES SIZE(SizeID),
 -- ) ON [PRIMARY]
 -- GO
+
+-- CREATE EMPLOYEE_TYPE TABLE
+IF EXISTS (SELECT * FROM sys.sysobjects WHERE Name = 'EMPLOYEE_TYPE')
+BEGIN
+   DROP TABLE EMPLOYEE_TYPE
+END
+CREATE TABLE [dbo].[EMPLOYEE_TYPE](
+   [EmployeeTypeID] [int] NOT NULL IDENTITY(1,1) PRIMARY KEY,
+   [EmpoyeeTypeName] [varchar](25) NOT NULL,
+   [EmployeeTypeDescription] [varchar](300) NOT NULL,
+   [WagePerHour] [Decimal(5,2)] NOT NULL
+) ON [PRIMARY]
+GO
+
+-- CREATE GENDER TABLE
+IF EXISTS (SELECT * FROM sys.sysobjects WHERE Name = 'GENDER')
+BEGIN
+   DROP TABLE GENDER
+END
+CREATE TABLE [dbo].[GENDER](
+   [GenderID] [int] NOT NULL IDENTITY(1,1) PRIMARY KEY,
+   [GenderName] [varchar](25) NOT NULL
+) ON [PRIMARY]
+GO
+
+-- CREATE STORE TABLE
+IF EXISTS (SELECT * FROM sys.sysobjects WHERE Name = 'STORE')
+BEGIN
+   DROP TABLE STORE
+END
+CREATE TABLE [dbo].[STORE](
+   [StoreID] [int] NOT NULL IDENTITY(1,1) PRIMARY KEY,
+   [StoreName] [varchar](100) NOT NULL,
+   [StoreAddress] [varchar](300) NOT NULL,
+   [StoreOpeningTime] [Time] NOT NULL,
+   [StoreClosingTime] [Time] NOT NULL
+) ON [PRIMARY]
+GO
+
+-- CREATE SHIFT_TYPE TABLE
+IF EXISTS (SELECT * FROM sys.sysobjects WHERE Name = 'SHIFT_TYPE')
+BEGIN
+   DROP TABLE SHIFT_TYPE
+END
+CREATE TABLE [dbo].[SHIFT_TYPE](
+   [ShiftTypeID] [int] NOT NULL IDENTITY(1,1) PRIMARY KEY,
+   [ShiftTypeName] [varchar](100) NOT NULL,
+   [ShiftTypeDescription] [varchar](300) NOT NULL,
+   [ShiftTypeBeginTime] [Time] NOT NULL,
+   [ShiftTypeEndTime] [Time] NOT NULL
+) ON [PRIMARY]
+GO
+
+-- CREATE SHIFT TABLE
+IF EXISTS (SELECT * FROM sys.sysobjects WHERE Name = 'SHIFT')
+BEGIN
+   DROP TABLE SHIFT
+END
+CREATE TABLE [dbo].[SHIFT](
+   [ShiftID] [int] NOT NULL IDENTITY(1,1) PRIMARY KEY,
+   [ShiftTypeID] [int] NOT NULL FOREIGN KEY REFERENCES SHIFT_TYPE(ShiftTypeID),
+   [StoreID] [int] NOT NULL FOREIGN KEY REFERENCES STORE(StoreID),
+   [DateTime] [datetime] NOT NULL
+) ON [PRIMARY]
+GO
+
+-- CREATE EMPLOYEE TABLE
+IF EXISTS (SELECT * FROM sys.sysobjects WHERE Name = 'EMPLOYEE')
+BEGIN
+   DROP TABLE EMPLOYEE
+END
+CREATE TABLE [dbo].[EMPLOYEE](
+   [EmployeeID] [int] NOT NULL IDENTITY(1,1) PRIMARY KEY,
+   [EmployeeTypeID] [int] NOT NULL FOREIGN KEY REFERENCES EMPLOYEE_TYPE(EmployeeTypeID),
+   [GenderID] [int] NOT NULL FOREIGN KEY REFERENCES GENDER(GenderID),
+   [EmployeeFName] [varchar](100) NOT NULL,
+   [EmployeeLName] [varchar](100) NOT NULL,
+   [EmployeeDOB] [date] NOT NULL,
+) ON [PRIMARY]
+GO
+
+-- CREATE SHIFT_EMPLOYEE TABLE
+IF EXISTS (SELECT * FROM sys.sysobjects WHERE Name = 'SHIFT_EMPLOYEE')
+BEGIN
+   DROP TABLE SHIFT_EMPLOYEE
+END
+CREATE TABLE [dbo].[SHIFT_EMPLOYEE](
+   [ShiftEmployeeID] [int] NOT NULL IDENTITY(1,1) PRIMARY KEY,
+   [ShiftID] [int] NOT NULL FOREIGN KEY REFERENCES SHIFT(ShiftID),
+   [EmployeeID] [int] NOT NULL FOREIGN KEY REFERENCES EMPLOYEE(EmployeeID)
+) ON [PRIMARY]
+GO
