31193d3d457fd887b2b92d83000fc1c3226b3c64
ANNIE LIU <annie.liu121@gmail.com>
2021-11-24 01:27:57 -0800

    Added computed columns

--- computed_cols_project04.sql
+++ computed_cols_project04.sql
@@ -55,7 +55,7 @@ AS
 BEGIN
 DECLARE @RET INT = (SELECT (COUNT(DO.DrinkOrderID)) 
                         FROM CUSTOMER C 
-                        JOIN ORDER O ON O.CustomerID = C.CustomerID
+                        JOIN [ORDER] O ON O.CustomerID = C.CustomerID
                         JOIN DRINK_ORDER DO ON DO.OrderID = O.OrderID
                         WHERE O.CustomerID = @PK
                         GROUP BY O.CustomerID)
@@ -64,4 +64,39 @@ END
 GO
 ALTER TABLE [CUSTOMER]
 ADD TotalDrinksCustomer AS (dbo.fn_TotalDrinksCustomer (CustomerID))
+GO
+
+-- Total hours worked by an employee
+CREATE FUNCTION fn_TotalHoursEmployee (@PK INT)
+RETURNS INT
+AS
+BEGIN
+DECLARE @RET INT = (SELECT (SUM(ST.ShiftTypeEndTime - ST.ShiftTypeBeginTime))
+                    FROM EMPLOYEE E
+                    JOIN SHIFT_EMPLOYEE SE ON E.EmployeeID = SE.EmployeeID
+                    JOIN SHIFT SH ON SE.ShiftID = SH.ShiftID
+                    JOIN SHIFT_TYPE ST ON SH.ShiftTypeID = ST.ShiftTypeID
+                    WHERE E.EmployeeID = @PK
+                    GROUP BY E.EmployeeID)
+RETURN @RET
+END
+GO
+ALTER TABLE EMPLOYEE
+ADD TotalHoursWorked AS (dbo.fn_TotalHoursEmployee (EmployeeID))
+GO
+
+-- Total ingredients in each drink
+CREATE FUNCTION fn_TotalIngredientsDrink (@PK INT)
+RETURNS INT
+AS 
+BEGIN
+DECLARE @RET INT = (SELECT (COUNT(DI.IngredientID))
+                    FROM DRINK_INGREDIENT DI
+                    WHERE DI.DrinkID = @PK
+                    GROUP BY DI.DrinkID)
+RETURN @RET
+END
+GO
+ALTER TABLE DRINK
+ADD TotalIngredients AS (dbo.fn_TotalIngredientsDrink (DrinkID))
 GO
