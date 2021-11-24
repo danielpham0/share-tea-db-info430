8548df5ca7c5df09f67ff8d2787ea74b76f824b4
ANNIE LIU <annie.liu121@gmail.com>
2021-11-24 00:30:26 -0800

    Added complex queries and ToppingID to DRINK_TOPPING_ORDER

--- complex_queries_project04.sql
+++ complex_queries_project04.sql
@@ -77,8 +77,37 @@ JOIN EMPLOYEE_TYPE ET ON ET.EmployeeTypeID = E.EmployeeTypeID
 JOIN SHIFT_EMPLOYEE SE ON SE.EmployeeID = E.EmployeeID
 JOIN SHIFT S ON S.ShiftID = SE.ShiftID
 JOIN SHIFT_TYPE ST ON ST.ShiftTypeID = S.ShiftTypeID
-JOIN ORDER O ON O.EmployeeID = E.EmployeeID
-WHERE ET.EmployeeTypeName = "Barista" 
+JOIN [ORDER] O ON O.EmployeeID = E.EmployeeID
+WHERE ET.EmployeeTypeName = 'Barista'
 GROUP BY E.EmployeeID, E.EmployeeFName, E.EmployeeLName, ET.EmployeeTypeName
 ORDER BY EmpAvgOrderPHour_Rank ASC
+GO
+
+-- For each store, what drinks are the most popular?
+
+SELECT DO.DrinkID, D.DrinkName, S.StoreName, COUNT(DO.OrderID) AS NumOrders,
+DENSE_RANK() OVER (PARTITION BY S.StoreName ORDER BY COUNT(DO.OrderID) DESC) AS DenseRankDrinkPopularity
+FROM STORE S
+    JOIN SHIFT SH ON S.StoreID = SH.StoreID
+    JOIN SHIFT_EMPLOYEE SE ON SH.ShiftID = SE.ShiftID
+    JOIN EMPLOYEE E ON SE.EmployeeID = E.EmployeeID
+    JOIN [ORDER] O ON E.EmployeeID = O.EmployeeID
+    JOIN DRINK_ORDER DO ON O.OrderID = DO.OrderID
+    JOIN DRINK D ON DO.DrinkID = D.DrinkID
+GROUP BY DO.DrinkID, D.DrinkName, S.StoreName
+GO
+
+-- For each store, what toppings are the most popular?
+
+SELECT DTO.ToppingID, T.ToppingName, S.StoreName, COUNT(DTO.DrinkOrderID) AS NumDrinkOrders,
+DENSE_RANK() OVER (PARTITION BY S.StoreName ORDER BY COUNT(DTO.DrinkOrderID) DESC) AS DenseRankToppingPopularity
+FROM STORE S
+    JOIN SHIFT SH ON S.StoreID = SH.StoreID
+    JOIN SHIFT_EMPLOYEE SE ON SH.ShiftID = SE.ShiftID
+    JOIN EMPLOYEE E ON SE.EmployeeID = E.EmployeeID
+    JOIN [ORDER] O ON E.EmployeeID = O.EmployeeID
+    JOIN DRINK_ORDER DO ON O.OrderID = DO.OrderID
+    JOIN DRINK_TOPPING_ORDER DTO ON DO.DrinkOrderID = DTO.DrinkOrderID
+    JOIN TOPPING T ON DTO.ToppingID = T.ToppingID
+GROUP BY DTO.ToppingID, T.ToppingName, S.StoreName
 GO
