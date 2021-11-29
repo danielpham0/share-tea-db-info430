31532942bf17ca00ab3df400e556202726058f78
ANNIE LIU <annie.liu121@gmail.com>
2021-11-28 23:35:17 -0800

    Added views for sorting drinks into categories

--- views_project04.sql
+++ views_project04.sql
@@ -1,6 +1,40 @@
 USE INFO_430_Proj_04
 GO
 
+-- Sort ordered drinks into cases based on number of toppings ordered and size
+-- 1) Above 2 toppings and size large -- Extravagant
+-- 2) Between 1 or 2 toppings and size large or medium -- Average
+-- 4) No toppings and a size small or medium -- Frugal
+-- 5) Other
+
+CREATE VIEW vwDrinksByToppingCountAndSize
+AS
+SELECT (CASE
+    WHEN ToppingCount >= 3 AND SizeName = 'Large'
+        THEN 'Extravagant'
+    WHEN ToppingCount BETWEEN 2 AND 3 AND (SizeName = 'Large' or SizeName = 'Medium')
+        THEN 'Average'
+    WHEN ToppingCount <= 1 AND (SizeName = 'Medium' or SizeName = 'Small')
+        THEN 'Frugal'
+    ELSE 'Other'
+        END) AS LabelForDrinks, COUNT(*) AS NumberOfDrinks
+FROM
+(SELECT DO.DrinkOrderID, S.SizeName, COUNT(DTO.ToppingID) AS ToppingCount 
+    FROM DRINK_ORDER DO 
+    JOIN SIZE S ON DO.SizeID = S.SizeID
+    LEFT JOIN DRINK_TOPPING_ORDER DTO on DO.DrinkOrderID = DTO.DrinkOrderID
+    GROUP BY DO.DrinkOrderID, S.SizeName) AS A
+GROUP BY (CASE
+    WHEN ToppingCount >= 3 AND SizeName = 'Large'
+        THEN 'Extravagant'
+    WHEN ToppingCount BETWEEN 2 AND 3 AND (SizeName = 'Large' or SizeName = 'Medium')
+        THEN 'Average'
+    WHEN ToppingCount <= 1 AND (SizeName = 'Medium' or SizeName = 'Small')
+        THEN 'Frugal'
+    ELSE 'Other'
+        END)
+GO 
+
 -- For each store, what drinks are the most popular?
 
 CREATE VIEW vwPopularDrinksByStore
